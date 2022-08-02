d9493a0eec5d76e3289ce9788761d61452084836
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-02 12:51:18 +0100

    Weather Preview UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import { Typography } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <Typography>
-        Weather App
-      </Typography>
+      <div className="box">
+        <div className="box-2-of-3">
+          <div className="weather-preview">
+            <a href='/' className="logo-text">Weather App</a>
+            <div className="weather-details">
+              <h1 className="weather-value">23°C</h1>
+              <div className="weather-con">
+                <h2 className="weather-location">Lagos</h2>
+                <p className="time-date">18:04 - Friday, 15 Apr</p>
+              </div>
+              <p className="weather-type">Cloudy</p>
+            </div>
+          </div>
+        </div>
+        <div className="box-1-of-3">
+          <div className="weather-select">
+            <input type="text" className="location-field" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
