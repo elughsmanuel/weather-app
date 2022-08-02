c6b3a0e74d9e7ae7e6487940b2d543e0df12bf0f
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-08-02 14:48:31 +0100

    Weather Preview Functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { format } from 'date-fns';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=887d88a95a3a6638d5096df07a1ad62f`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      setLocation('');
+    }
+  }
+
   return (
     <div className="App">
       <div className="box">
@@ -8,18 +28,22 @@ function App() {
           <div className="weather-preview">
             <a href='/' className="logo-text">Weather App</a>
             <div className="weather-details">
-              <h1 className="weather-value">23°C</h1>
+              <h1 className="weather-value">{data.main ? data.main.temp.toFixed() : null}°C</h1>
               <div className="weather-con">
-                <h2 className="weather-location">Lagos</h2>
-                <p className="time-date">18:04 - Friday, 15 Apr</p>
+                <h2 className="weather-location">{data.name}</h2>
+                <p className="time-date">{ format(new Date(), 'H:m - EEEE, d MMM')}</p>
               </div>
-              <p className="weather-type">Cloudy</p>
+              <div className="weather-con-2">
+                <p className="weather-type">{data.weather ? data.weather[0].description : null}</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="box-1-of-3">
           <div className="weather-select">
-            <input type="text" className="location-field" />
+            <input onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            type="text" value={location} className="location-field" />
           </div>
         </div>
       </div>
